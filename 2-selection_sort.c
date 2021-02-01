69ed45a66b64d4c8d9dffcd783206199d1454acb
peytonbrsmith <peytonbrsmith@gmail.com>
2021-02-01 13:06:16 -0600

    updated selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,6 +10,9 @@ void selection_sort(int *array, size_t size)
 {
 	unsigned long int i, j, min_index;
 
+	if (array == NULL)
+		return;
+
 	for (i = 0; i < (size - 1) && size > 2; i++)
 	{
 		min_index = i;
